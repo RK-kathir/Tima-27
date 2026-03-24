bc97b1411e6cc80a64ad970b5a072fce1dc9a80b
RK-kathir <kvelkvp@gmail.com>
2026-03-24 14:43:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: Replace 'tima-2027' with your EXACT GitHub repository name
+  // Make sure to keep the forward slashes before and after!
+  base: '/tima-2027/', 
+})
