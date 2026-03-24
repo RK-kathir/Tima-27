2a813334d5ad6a3e72697a946cd29c2e5fd754a4
RK-kathir <kvelkvp@gmail.com>
2026-03-24 15:08:43 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,6 @@ const Footer = () => {
       <p>Phone: +91 94893 07781</p>
       <hr className="footer-divider" />
       <p>&copy; 2027 MIT Campus, Anna University. All Rights Reserved.</p>
-      <p className="signature">BUILT WITH <span>&hearts;</span> BY KATHIRVEL R</p>
     </footer>
   );
 };
