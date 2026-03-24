b6fedc941fa7ff0e5bc8e7c44d2237d979ff43d7
RK-kathir <kvelkvp@gmail.com>
2026-03-24 14:38:46 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
 
+const Footer = () => {
+  return (
+    <footer id="contact">
+      <h3>Contact Us</h3>
+      <p>Organizing Secretary, TIMA 2027</p>
+      <p>Email: <strong>ie.tima2027@gmail.com</strong></p>
+      <p>Phone: +91 94893 07781</p>
+      <hr className="footer-divider" />
+      <p>&copy; 2027 MIT Campus, Anna University. All Rights Reserved.</p>
+      <p className="signature">BUILT WITH <span>&hearts;</span> BY KATHIRVEL R</p>
+    </footer>
+  );
+};
+
+export default Footer;
